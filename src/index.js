54b8edc500f0aaffc8453cbec9f5f392ca6249b6
Lichapa <lichapau@gmail.com>
2021-11-04 16:14:17 +0200

    Removed hardcoded array objects

--- src/index.js
+++ src/index.js
@@ -28,43 +28,4 @@ function loadTodo() {
   toDoList.innerHTML = toDoListItems;
 }
 
-function todo() {
-  const toDoObjects = [
-    {
-      task: 'Clean the house',
-      completed: false,
-      index: 0,
-    },
-    {
-      task: 'sleep',
-      completed: true,
-      index: 1,
-    },
-    {
-      task: 'Study',
-      completed: false,
-      index: 2,
-    },
-  ];
-
-  if (localStorage.getItem('todos') === null) {
-    let toDoListItems = '';
-    for (const toDo of toDoObjects) {
-      if (toDo.completed === true) {
-        toDoListItems += `<li class='task flex-end'>
-        <div class='content completed flex-end' id='${toDo.index}'> <input type='checkbox' class='check-box' checked>
-        <p> ${toDo.task}</p></div><i class="fas fa-ellipsis-v"></i></li>`;
-      } else {
-        toDoListItems += `<li class='task flex-end '>
-        <div class='content flex-end' id='${toDo.index}'> <input type='checkbox' class='check-box'><p> ${toDo.task} </p>
-        </div><i class="fas fa-ellipsis-v"></i></li>`;
-      }
-      saveTasks(toDo);
-    }
-    toDoList.innerHTML = toDoListItems;
-  } else {
-    loadTodo();
-  }
-}
-
-document.addEventListener('DOMContentLoaded', todo);
+document.addEventListener('DOMContentLoaded', loadTodo);
